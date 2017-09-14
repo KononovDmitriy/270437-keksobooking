4400b3947e16b66af2e04f7775ade1e1d094030a
Кононов Дмитрий Сергеевич <kds@ad.atnn.ru>
2017-09-14 15:51:25 +0300

    'v2_added_pictures_to_the_card'

--- js/card.js
+++ js/card.js
@@ -20,8 +20,19 @@
     });
   }
 
+  function getPhotoElement(photoUrl) {
+    var lodgePhoto = document.createElement('img');
+    lodgePhoto.setAttribute('src', photoUrl);
+    lodgePhoto.setAttribute('alt', 'Lodge photo');
+    lodgePhoto.setAttribute('width', '52px');
+    lodgePhoto.setAttribute('height', '42px');
+
+    return lodgePhoto;
+  }
+
   function createDialogPanelFromTemplate(ads) {
     var template = lodgeTemplate.content.cloneNode(true);
+    var lodgePhotos = template.querySelector('.lodge__photos');
 
     template.querySelector('.lodge__title').textContent = ads.offer.title;
     template.querySelector('.lodge__address').textContent = ads.offer.address;
@@ -35,7 +46,12 @@
         'Заезд после ' + ads.offer.checkin + ', выезд до ' + ads.offer.checkout;
     template.querySelector('.lodge__description').textContent =
         ads.offer.description;
+
     dialogTitleImg.src = ads.author.avatar;
+
+    ads.offer.photos.forEach(function (photoUrl) {
+      lodgePhotos.appendChild(getPhotoElement(photoUrl));
+    });
 
     createFeaturesElements(template, ads.offer.features);
     return template;
